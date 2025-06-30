400d801405569285f7333e2c389f9b1d3d6ba5d6
Piyush Vishwakarma <115068496+Piyushvishwak@users.noreply.github.com>
2025-06-30 23:21:00 +0530

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("Connected to MongoDB");
+});
+
+const LoginSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    }
+})
+const collection = new mongoose.model("users", LoginSchema);
+module.exports = collection;
